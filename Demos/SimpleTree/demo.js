4dee6c000fa9ff53192ecc660dae4b1514632935
CNSKnight <Steeve@dharmiWeb.net>
2017-02-10 14:19:22 -0500

    formatting

--- Demos/SimpleTree/demo.js
+++ Demos/SimpleTree/demo.js
@@ -1,64 +1,60 @@
-window.addEvent('domready',function(){
-	tree = new Mif.Tree({
-		container: $('tree-container'),// tree container
-		types: {// node types
-			folder:{
-				openIcon: 'mt-open-icon',//css class open icon
-				closeIcon: 'mt-close-icon'// css class close icon
-			}
-		},
-		dfltType:'folder',//default node type
-		height: 18//node height
-	});
+window.addEvent('domready', function() {
+    var tree = new Mif.Tree({
+        container: $('tree-container'), // tree container
+        types: { // node types
+            folder: {
+                openIcon: 'mt-open-icon', //css class open icon
+                closeIcon: 'mt-close-icon' // css class close icon
+            }
+        },
+        dfltType: 'folder', //default node type
+        height: 18 //node height
+    });
 
-	var json=[
-		{
-			"property": {
-				"name": "root"
-			},
-			"children": [
-				{
-					"property": {
-						"name": "node1"
-					}
-				},
-				{
-					"property": {
-						"name": "node2"
-					},
-					"state": {
-						"open": true
-					},
-					"children":[
-						{
-							"property": {
-								"name": "node2.1"
-							}
-						},
-						{
-							"property": {
-								"name": "node2.2"
-							}
-						}
-					]
-				},
-				{
-					"property": {
-						"name": "node4"
-					}
-				},
-				{
-					"property": {
-						"name": "node3"
-					}
-				}
-			]
-		}
-	];
-	
-	// load tree from json.
-	tree.load({
-		json: json
-	});
-	
+    var json = [{
+        "property": {
+            "name": "root"
+        },
+        "children": [{
+                "property": {
+                    "name": "node1"
+                }
+            },
+            {
+                "property": {
+                    "name": "node2"
+                },
+                "state": {
+                    "open": true
+                },
+                "children": [{
+                        "property": {
+                            "name": "node2.1"
+                        }
+                    },
+                    {
+                        "property": {
+                            "name": "node2.2"
+                        }
+                    }
+                ]
+            },
+            {
+                "property": {
+                    "name": "node4"
+                }
+            },
+            {
+                "property": {
+                    "name": "node3"
+                }
+            }
+        ]
+    }];
+
+    // load tree from json.
+    tree.load({
+        json: json
+    });
+
 });
